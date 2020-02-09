f7cf6e5f68056ded1cbe3335e9af8d40638a4885
Pete Lindsell <pete.lindsell@gmail.com>
2020-02-09 17:39:34 +0000

    Added cypress.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
-const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-
-const config = {
+module.exports = {
+  entry: './docs/index.js',
   module: {
     rules: [
       {
@@ -30,43 +29,87 @@ const config = {
       },
     ],
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.json'],
-  },
-};
-
-const libConfig = {
-  entry: './components',
-  output: {
-    path: path.join(__dirname, 'lib'),
-    filename: 'index.js',
-    library: 'crukReactComponents',
-    libraryTarget: 'umd'
-  },
-  externals:{
-    react: {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react'
-    },
-  },
-  devtool: 'source-map',
-  ...config,
-};
-
-const docsConfig = {
-  entry: './docs',
-  output: {
-    path: path.join(__dirname, 'public'),
-  },
   plugins: [
     new HtmlWebPackPlugin({
       template: './docs/index.html',
       filename: './index.html',
     }),
   ],
-  ...config,
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+  },
 };
 
-module.exports = [libConfig, docsConfig];
+
+// const path = require('path');
+// const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+// const config = {
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//         },
+//       },
+//       {
+//         test: /\.tsx?$/,
+//         exclude: /node_modules/,
+//         use: 'ts-loader',
+//       },
+//       {
+//         test: /\.html$/,
+//         use: [
+//           {
+//             loader: 'html-loader',
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.mdx?$/,
+//         use: ['babel-loader', '@mdx-js/loader'],
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: ['.tsx', '.ts', '.js', '.json'],
+//   },
+// };
+
+// const libConfig = {
+//   entry: './components',
+//   output: {
+//     path: path.join(__dirname, 'lib'),
+//     filename: 'index.js',
+//     library: 'crukReactComponents',
+//     libraryTarget: 'umd'
+//   },
+//   externals:{
+//     react: {
+//       root: 'React',
+//       commonjs2: 'react',
+//       commonjs: 'react',
+//       amd: 'react'
+//     },
+//   },
+//   devtool: 'source-map',
+//   ...config,
+// };
+
+// const docsConfig = {
+//   entry: './docs',
+//   output: {
+//     path: path.join(__dirname, 'public'),
+//   },
+//   plugins: [
+//     new HtmlWebPackPlugin({
+//       template: './docs/index.html',
+//       filename: './index.html',
+//     }),
+//   ],
+//   ...config,
+// };
+
+// module.exports = [libConfig, docsConfig];
