3d4caad97220ea1bbbb71d702c4a28277bb73d12
Pete Lindsell <pete.lindsell@gmail.com>
2020-02-09 21:29:23 +0000

    Added keyboard accessibility test for modal.

---
+++ cypress/integration/snapshots.spec.js
@@ -0,0 +1,66 @@
+const components = [
+  'Avatar',
+  'Badge',
+  'Box',
+  'Button',
+  'Checkbox',
+  'Collapse',
+  'ErrorText',
+  'Heading',
+  'Icon',
+  'Loader',
+  'Modal',
+  'Pagination',
+  'PopOver',
+  'ProgressBar',
+  'Radio',
+  'Select',
+  'Step',
+  'TextField',
+  'Totaliser',
+  'UserBlock',
+];
+
+const selectComponent = (componentName, brand) => {
+  cy.contains('a', componentName).click();
+  cy.get('head').invoke(
+    'append',
+    '<style type="text/css">header {display: none;}</style>',
+  );
+  switch (componentName) {
+    case 'Modal':
+      cy.contains('Show me a modal').click();
+      cy.matchImageSnapshot(`${brand}_${componentName}`);
+      break;
+    case 'PopOver':
+      cy.contains('Share left').click();
+      cy.get('[aria-label="Example code preview"]')
+        .first()
+        .matchImageSnapshot(`${brand}_${componentName}`);
+      break;
+    case 'Collapse':
+      cy.contains('What is Lorem Ipsum?').click();
+      cy.wait(500); // Wait to expand
+      cy.get('[aria-label="Example code preview"]')
+        .first()
+        .matchImageSnapshot(`${brand}_${componentName}`);
+      break;
+    default:
+      cy.get('[aria-label="Example code preview"]')
+        .first()
+        .matchImageSnapshot(`${brand}_${componentName}`);
+  }
+};
+
+components.forEach(componentName => {
+  it(`CRUK ${componentName} Should match snapshot`, () => {
+    cy.visit('/');
+    selectComponent(componentName, 'cruk');
+  });
+
+  it(`SU2C ${componentName} Should match snapshot`, () => {
+    cy.visit('/');
+    cy.contains('button', 'Switch theme').click();
+    selectComponent(componentName, 'su2c');
+  });
+});
