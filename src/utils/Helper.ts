b24f31c72bd7fb9a96df471f81b579d2ab0ec092
Elliot Revan <elliotrevan@gmail.com>
2020-02-27 16:18:44 +0000

    fix value for snapshots

--- src/utils/Helper.ts
+++ src/utils/Helper.ts
@@ -9,7 +9,7 @@ export const calculatePercentRounded = (value: number, target: number): number =
 export const formatMoney = (value: number): string => {
   const parsed = Number.parseFloat((Math.round(value * 100) / 100).toString()).toFixed(2);
   if (Number.isNaN(parseFloat(parsed)) || isNaN(value)) {
-    return '0.0';
+    return '0';
   }
   return parsed;
 };
