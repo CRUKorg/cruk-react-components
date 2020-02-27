804ee8b57ffb0bd85fe7604d2459ef4050386cae
Elliot Revan <elliotrevan@gmail.com>
2020-02-27 15:02:53 +0000

    update lib

--- lib/components/Radio/index.d.ts
+++ lib/components/Radio/index.d.ts
@@ -1,13 +1,18 @@
 import React from 'react';
-declare type RadioGroupProps = {
-    legend: string;
-    attributes: Array<{
-        value: string;
-        option: string;
-    }>;
-    onChange: React.ChangeEventHandler<HTMLInputElement>;
-    checked: string;
+declare type RadioProps = {
+    className?: string;
+    checked: boolean;
+    disabled?: boolean;
     name: string;
+    onChange: React.ChangeEventHandler<HTMLInputElement>;
+    theme?: {
+        colors: {};
+        utilities: {};
+    };
+    value: string;
+    children: any;
 };
-declare const RadioGroup: (props: RadioGroupProps) => JSX.Element;
-export default RadioGroup;
+declare const _default: React.ForwardRefExoticComponent<Pick<RadioProps, "name" | "children" | "disabled" | "checked" | "onChange" | "value" | "className"> & {
+    theme?: any;
+}>;
+export default _default;
