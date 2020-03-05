b8a74e25d84e67cb82a295efc1f049d7e22a0d21
Elliot Revan <elliotrevan@gmail.com>
2020-03-05 11:49:51 +0000

    fix header types

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -75,12 +75,12 @@ const H6 = styled.h6`
 `;
 
 type Props = {
-  h1: boolean;
-  h2: boolean;
-  h3: boolean;
-  h4: boolean;
-  h5: boolean;
-  h6: boolean;
+  h1?: boolean;
+  h2?: boolean;
+  h3?: boolean;
+  h4?: boolean;
+  h5?: boolean;
+  h6?: boolean;
 };
 
 const Heading: FunctionComponent<Props> = props => {
