a4f70d22c83c5545af82c4be64b572be4cc7dc06
David Collard <david.collard@cancer.org.uk>
2020-04-16 13:09:51 +0100

    Select: make hintText optional

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -49,7 +49,7 @@ type SelectProps = SelectHTMLAttributes<{}> &
     theme: ThemeType;
     label: string;
     value: string;
-    hintText: string;
+    hintText?: string;
   };
 
 const Select: FunctionComponent<SelectProps> = ({
