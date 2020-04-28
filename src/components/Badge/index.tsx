f79483d0596e380593a967c129b4c4a447551d52
Elliot Revan <elliotrevan@gmail.com>
2020-04-28 13:19:15 +0100

    make badge correct size

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -2,25 +2,34 @@ import React, { FunctionComponent } from 'react';
 import styled, { css, withTheme } from 'styled-components';
 
 import defaultTheme from '../../themes/cruk';
-import { ThemeType, ColorsType } from '../../themes/types';
+import { ThemeType, SpaceType } from '../../themes/types';
 
 type BadgeProps = {
   backgroundColor?: string;
   text: boolean;
   theme?: ThemeType;
-  getBackgroundColor?: string;
-  size?: number;
+  size?: SpaceType;
   children?: any;
 };
 
 // TODO Look at where 15 comes from in the height and width bellow.
 const StyledBadge = styled.span`
-  background-color: ${props => props.getBackgroundColor};
+  background-color: ${({ theme: { colors }, backgroundColor }) =>
+    backgroundColor && typeof colors[backgroundColor] !== 'undefined'
+      ? colors[backgroundColor]
+      : backgroundColor
+      ? backgroundColor
+      : colors['primary']};
   color: ${props => props.theme.colors.textLight};
   text-align: center;
   border-radius: 1.5rem;
   font-size: ${props => props.theme.fontSizes.small};
-  padding: 3px 10px;
+  line-height: 1rem;
+  padding: ${({
+    theme: {
+      spacing: { extraExtraSmall },
+    },
+  }: BadgeProps) => extraExtraSmall};
   display: inline-block;
 
   ${(props: BadgeProps) =>
@@ -29,30 +38,41 @@ const StyledBadge = styled.span`
       padding: 0;
       border-radius: 50%;
       display: block;
-      height: ${props.size + 15}px;
-      width: ${props.size + 15}px;
-      line-height: ${props.size + 15}px;
+      height: ${({
+        size,
+        theme: {
+          spacing,
+          spacing: { extraSmall },
+        },
+      }: BadgeProps) => `calc(${spacing[size]} + ${extraSmall})`};
+      width: ${({
+        size,
+        theme: {
+          spacing,
+          spacing: { extraSmall },
+        },
+      }: BadgeProps) => `calc(${spacing[size]} + ${extraSmall})`};
+      line-height: ${({
+        size,
+        theme: {
+          spacing,
+          spacing: { extraSmall },
+        },
+      }: BadgeProps) => `calc(${spacing[size]} + ${extraSmall})`};
       svg {
-        height: ${props.size}px;
+        height: ${({ size, theme: { spacing } }: BadgeProps) => spacing[size]};
       }
     `}
 `;
 
 const Badge: FunctionComponent<BadgeProps> = props => {
-  const { backgroundColor, text, theme, getBackgroundColor, size, children, ...rest } = props;
+  const { text, theme, size, children, ...rest } = props;
   const mergedTheme = {
     ...defaultTheme,
     ...theme,
   };
-  const checkBackgroundColor = (mergedTheme.colors as ColorsType)[backgroundColor] || backgroundColor;
   return (
-    <StyledBadge
-      theme={mergedTheme}
-      text={typeof children === 'string'}
-      size={15}
-      getBackgroundColor={backgroundColor !== undefined ? checkBackgroundColor : mergedTheme.colors.primary}
-      {...rest}
-    >
+    <StyledBadge theme={mergedTheme} text={typeof children === 'string'} size="extraSmall" {...rest}>
       {children}
     </StyledBadge>
   );
