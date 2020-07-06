9ce4869bf4c5605a2ecb233d05155af45d91ee90
Elliot Revan <elliot.revan@gmail.com>
2020-07-06 14:54:24 +0100

    rem me ems

--- src/themes/cruk.ts
+++ src/themes/cruk.ts
@@ -146,7 +146,7 @@ export const SPACING: SpacingType = {
 export const FONT_SIZES: FontSizesType = {
   extraSmall: '0.75rem', // 12px
   small: '0.857rem', // 14px
-  medium: '1em', // 16px
+  medium: '1rem', // 16px
   large: '1.25rem', // 20px
   extraLarge: '1.5625rem', // 25px
   extraExtraLarge: '2rem', // 32px
