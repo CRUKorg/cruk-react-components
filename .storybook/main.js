1653a3f5252349111e543114dc7f6276c483544d
Elliot Revan <elliot.revan@gmail.com>
2020-08-03 16:20:30 +0100

    storybook working locally with typescript

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  stories: ['../src/**/__tests__/*.stories.[tj]s[x]'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+};
---
+++ src/components/Button/__tests__/Button.stories.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { ThemeProvider } from 'styled-components';
+
+//components
+import { Box, Icon, Button, crukTheme, crukTheme2, su2cTheme } from '../../index';
+
+const buttonContent = () => (
+  <Box>
+    <Box>
+      <Button onClick={action('clicked')}>Standard button</Button>
+    </Box>
+    <Box>
+      <Button disabled={true} onClick={action('clicked')}>
+        Standard disabled button
+      </Button>
+    </Box>
+    <Box>
+      <Button appearance="primary" onClick={action('clicked')}>
+        Primary button
+      </Button>
+    </Box>
+    <Box>
+      <Button appearance="primary" disabled={true} onClick={action('clicked')}>
+        Primary disabled button
+      </Button>
+    </Box>
+    <Box>
+      <Button appearance="Secondary" onClick={action('clicked')}>
+        Secondary button
+      </Button>
+    </Box>
+    <Box>
+      <Button appearance="Secondary" disabled={true} onClick={action('clicked')}>
+        Secondary disabled button
+      </Button>
+    </Box>
+    <Box>
+      <Button size="large" onClick={action('clicked')}>
+        Large button
+      </Button>
+    </Box>
+    <Box>
+      <Button onClick={action('clicked')}>
+        <Icon name="view" />
+        Icon with text
+      </Button>
+    </Box>
+    <Box>
+      <Button onClick={action('clicked')}>
+        Icon right
+        <Icon name="edit" />
+      </Button>
+    </Box>
+    <Box>
+      <Button onClick={action('clicked')}>
+        <Icon name="view" />
+        Icon either side
+        <Icon name="view" />
+      </Button>
+    </Box>
+    <Box>
+      <Button href="https://www.styled-components.com/" onClick={action('clicked')}>
+        Link as Button
+      </Button>
+    </Box>
+    <Box>
+      <Button appearance="text" onClick={action('clicked')}>
+        Text style button
+      </Button>
+    </Box>
+    <Box>
+      <Button aria-label="Upload a photo">
+        <Icon name="uploadPhoto" />
+      </Button>
+    </Box>
+    <Box>
+      <Button
+        css="background-color: #4267b2;border-color: #4267b2; color: white; :hover {background-color: #365899; color: white;}"
+        onClick={action('clicked')}
+      >
+        <Icon name="facebookSquare" size="18px" />
+        Continue with facebook
+      </Button>
+    </Box>
+    <Box>
+      <Button full={true} onClick={action('clicked')}>
+        Full width Button
+      </Button>
+    </Box>
+  </Box>
+);
+
+storiesOf('Button', module)
+  .add('CRUK buttons', () => <ThemeProvider theme={crukTheme}>{buttonContent()}</ThemeProvider>)
+  .add('CRUK2 buttons', () => <ThemeProvider theme={crukTheme2}>{buttonContent()}</ThemeProvider>)
+  .add('su2c buttons', () => <ThemeProvider theme={su2cTheme}>{buttonContent()}</ThemeProvider>);
