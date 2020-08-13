8eca94f4b569e8433829f0a9039191cc3baf934c
Elliot Revan <elliot.revan@gmail.com>
2020-08-13 14:10:37 +0100

    first badge test working with snapshot

---
+++ src/components/TestWrapper.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './GlobalStyle';
+import crukTheme from '../themes/cruk';
+import crukTheme2 from '../themes/cruk2';
+import su2cTheme from '../themes/su2c';
+import Heading from './Heading';
+
+export const TestWrapper: FC = ({ children }) => (
+  <>
+    <ThemeProvider theme={crukTheme}>
+      <GlobalStyle />
+      <Heading>CRUK Theme</Heading>
+      {children}
+    </ThemeProvider>
+    <ThemeProvider theme={crukTheme2}>
+      <GlobalStyle />
+      <Heading>CRUK 2 Theme</Heading>
+      {children}
+    </ThemeProvider>
+    <ThemeProvider theme={su2cTheme}>
+      <GlobalStyle />
+      <Heading>SU2C Theme</Heading>
+      {children}
+    </ThemeProvider>
+  </>
+);
+
+export default TestWrapper;
