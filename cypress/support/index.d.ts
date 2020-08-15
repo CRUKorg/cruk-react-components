bb2a0b400cef188b8d2668fa87b581ffdec752e5
Elliot Revan <elliot.revan@gmail.com>
2020-08-15 10:23:07 +0100

    fix chain-able types for cypress

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,8 +1,9 @@
 declare namespace Cypress {
   interface Chainable {
-    injectAxe(): void;
-    checkA11y(context?: any, options?: any, violationCallback?: any, skipFailures?: boolean): void;
-    matchImageSnapshot(nameOrOptions?: string | Object): void;
-    getInputByLabel(input: string): any;
+    injectAxe(): Chainable;
+    checkA11y(context?: any, options?: any, violationCallback?: any, skipFailures?: boolean): Chainable;
+    matchImageSnapshot(nameOrOptions?: string | Object): Chainable;
+    getInputByLabel(input: string): Chainable;
+    tab(options?: Partial<{ shift: Boolean }>): Chainable;
   }
 }
