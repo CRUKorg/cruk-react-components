5fb9f053620e31e70ddd6fbe619e1e33d554dfda
Lewis Prescott <Lewis.Prescott@cancer.org.uk>
2020-09-04 07:58:38 +0100

    install percy, remove image-snapshot

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,7 +1,5 @@
-const {
-  addMatchImageSnapshotPlugin,
-} = require('cypress-image-snapshot/plugin');
+let percyHealthCheck = require('@percy/cypress/task');
 
 module.exports = (on, config) => {
-  addMatchImageSnapshotPlugin(on, config);
+  on("task", percyHealthCheck);
 };
