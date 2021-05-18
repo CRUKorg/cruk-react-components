086d0629a0b03589a3308db033846f75cf668b15
Elliot <elliotrevan@gmail.com>
2021-05-18 08:41:30 +0100

    trying to get snapshotworking again

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,7 @@
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
+import { register } from 'cypress-match-screenshot';
+
+register();
 
 if (Cypress.config('isInteractive')) {
   Cypress.Commands.add('matchImageSnapshot', () => {
@@ -18,21 +21,27 @@ if (Cypress.config('isInteractive')) {
 // });
 
 // Return input or select by label text. Could be called getFormElementByLabel but that very long.
-Cypress.Commands.add('getInputByLabel', {
-    prevSubject: 'optional'
-    }, (subject, text)  => {
+Cypress.Commands.add(
+  'getInputByLabel',
+  {
+    prevSubject: 'optional',
+  },
+  (subject, text) => {
     if (subject) {
-        return (cy.get(subject).contains('label', text).then(($label) => {
-            const name = $label.attr('for');
-            if (name) return cy.get(`#${name}`);
-            cy.wrap($label).find('input, select, textarea');
-        }))
+      return cy
+        .get(subject)
+        .contains('label', text)
+        .then($label => {
+          const name = $label.attr('for');
+          if (name) return cy.get(`#${name}`);
+          cy.wrap($label).find('input, select, textarea');
+        });
     } else {
-        return (cy.contains('label', text).then(($label) => {
-            const name = $label.attr('for');
-            if (name) return cy.get(`#${name}`);
-            cy.wrap($label).find('input, select, textarea');
-        }))
+      return cy.contains('label', text).then($label => {
+        const name = $label.attr('for');
+        if (name) return cy.get(`#${name}`);
+        cy.wrap($label).find('input, select, textarea');
+      });
     }
-    }
+  },
 );
