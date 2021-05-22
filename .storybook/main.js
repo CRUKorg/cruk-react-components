01814e5e39ba8c922ddca2283a407d07dbf831da
Elliot <elliotrevan@gmail.com>
2021-05-22 19:57:24 +0100

    storybook with themes, controls and absolute paths for button

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/components/*/*.stories.mdx', '../src/components/*/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@react-theming/storybook-addon'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    config.resolve.modules = [...(config.resolve.modules || []), path.resolve('./')];
+    return config;
+  },
+};
