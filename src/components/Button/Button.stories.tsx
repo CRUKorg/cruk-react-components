89b10af35250815bcbb0b970b3a34028e463ff14
Elliot <elliotrevan@gmail.com>
2021-05-29 10:30:39 +0100

    SU2C themed button stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react';
 import Icon from 'src/components/Icon';
 import Button, { Props } from './';
+import { su2cTheme } from '..';
+import { ThemeProvider } from 'styled-components';
 
 export default {
   title: 'Button',
@@ -39,7 +41,6 @@ Tertiary.args = {
 
 const TemplateWithIcon: Story = args => (
   <Button {...args}>
-    {' '}
     <Icon name="clock" />A button
     <Icon name="clock" />
   </Button>
@@ -49,6 +50,43 @@ export const WithIcons: Story = TemplateWithIcon.bind({});
 WithIcons.args = {
   appearance: 'primary',
   disabled: false,
+  full: false,
+  size: 'm',
+  theme: su2cTheme,
+};
+
+const TemplateWithSU2C: Story = args => (
+  <ThemeProvider theme={su2cTheme}>
+    <Button {...args}>A button</Button>
+  </ThemeProvider>
+);
+
+export const SU2CPrimary: Story = TemplateWithSU2C.bind({});
+SU2CPrimary.storyName = 'SU2C Primary';
+SU2CPrimary.args = {
+  appearance: 'primary',
+  disabled: false,
+  children: 'A button',
+  full: false,
+  size: 'm',
+};
+
+export const SU2CSecondary: Story = TemplateWithSU2C.bind({});
+SU2CSecondary.storyName = 'SU2C Secondary';
+SU2CSecondary.args = {
+  appearance: 'secondary',
+  disabled: false,
+  children: 'A button',
+  full: false,
+  size: 'm',
+};
+
+export const SU2CTertiary: Story = TemplateWithSU2C.bind({});
+SU2CTertiary.storyName = 'SU2C Tertiary';
+SU2CTertiary.args = {
+  appearance: 'tertiary',
+  disabled: false,
+  children: 'A button',
   full: false,
   size: 'm',
 };
