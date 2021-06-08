3b05cd07446b3b59ff035bca1fececf48dd8d64f
Elliot <elliotrevan@gmail.com>
2021-06-08 21:30:10 +0100

    screen reader improvements

--- src/components/AddressLookup/index.tsx
+++ src/components/AddressLookup/index.tsx
@@ -55,7 +55,7 @@ const AddressLookup: FunctionComponent<AddressLookupProps> = forwardRef(
     ref?: Ref<HTMLInputElement>,
   ) => {
     const [addressOptions, setAddressOptions] = React.useState<AddressOptionsType[]>([]);
-    const [activeOption, setActiveOption] = React.useState(0);
+    const [activeOption, setActiveOption] = React.useState(-1);
     const wrapperRef = useRef<HTMLDivElement>(null);
     const foundTheme = useTheme();
     const theme = {
@@ -80,7 +80,7 @@ const AddressLookup: FunctionComponent<AddressLookupProps> = forwardRef(
     });
 
     const clearOptions = () => {
-      setActiveOption(0);
+      setActiveOption(-1);
       setAddressOptions([]);
     };
 
@@ -104,7 +104,6 @@ const AddressLookup: FunctionComponent<AddressLookupProps> = forwardRef(
           // Occasionally get the error "The query didn't respond fast enough, it may be too complex."
           // returned with a 200 response. Example query "n17 6t"
           if (data.Items[0].Error) throw new Error('Something went wrong please try again');
-          setActiveOption(0);
           setAddressOptions(data.Items || []);
         })
         .catch(err => onAddressError(err));
@@ -142,18 +141,21 @@ const AddressLookup: FunctionComponent<AddressLookupProps> = forwardRef(
           type="search"
           {...props}
           onKeyDown={e => {
-            if (e.keyCode === 13 && addressOptions[activeOption]) {
+            if (e.key === 'Enter' && addressOptions[activeOption]) {
               e.preventDefault();
               if (addressOptions[activeOption].Type === 'Address') return getAddress(addressOptions[activeOption].Id);
               search(addressOptions[activeOption].Text, addressOptions[activeOption].Id);
-            } else if (e.keyCode === 38) {
+              setActiveOption(-1);
+            } else if (e.key === 'ArrowUp') {
               e.preventDefault();
-              if (activeOption <= 0) return setActiveOption(addressOptions.length - 1);
+              if (activeOption <= -1) return setActiveOption(addressOptions.length - 1);
               setActiveOption(activeOption - 1);
-            } else if (e.keyCode === 40) {
+            } else if (e.key === 'ArrowDown') {
               e.preventDefault();
               if (activeOption + 1 >= addressOptions.length) return setActiveOption(0);
               setActiveOption(activeOption + 1);
+            } else {
+              setActiveOption(-1);
             }
           }}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
@@ -161,34 +163,41 @@ const AddressLookup: FunctionComponent<AddressLookupProps> = forwardRef(
             if (onChange) onChange(e);
           }}
         />
-        <ScreenReaderOnly role="status" aria-live="assertive">
-          {!!addressOptions.length &&
-            `We have found ${addressOptions.length} result${addressOptions.length !== 1 &&
-              's'} matching your search. Use up and down arrow keys to navigate`}
-        </ScreenReaderOnly>
         {!!addressOptions.length && (
-          <ListWrapper ref={wrapperRef}>
-            <List aria-label="found addresses" id="found_addresses" role="listbox" theme={theme}>
-              {addressOptions.map((address, index) => (
-                <ListItem
-                  id={`addressOptions-${index}`}
-                  isActive={index === activeOption}
-                  key={address.Id}
-                  onClick={() => {
-                    if (address.Type === 'Address') return getAddress(address.Id);
-                    search(address.Text, address.Id);
-                  }}
-                  role="option"
-                  theme={theme}
-                >
-                  <Text as="span">
-                    {address.Text} {address.Description}
-                  </Text>
-                  {address.Type !== 'Address' && <Icon name="chevronRight" />}
-                </ListItem>
-              ))}
-            </List>
-          </ListWrapper>
+          <>
+            <ScreenReaderOnly role="status" aria-live="assertive">
+              {!!addressOptions.length &&
+                `We have found ${addressOptions.length} result${addressOptions.length !== 1 &&
+                  's'} matching your search. Use up and down arrow keys to navigate`}
+            </ScreenReaderOnly>
+            <ListWrapper ref={wrapperRef}>
+              <List aria-label="found addresses" id="found_addresses" role="listbox" theme={theme}>
+                {addressOptions.map((address, index) => (
+                  <ListItem
+                    id={`addressOptions-${index}`}
+                    isActive={index === activeOption}
+                    key={address.Id}
+                    onClick={() => {
+                      if (address.Type === 'Address') return getAddress(address.Id);
+                      search(address.Text, address.Id);
+                    }}
+                    role="option"
+                    theme={theme}
+                  >
+                    <Text as="span">
+                      {address.Text} {address.Description}
+                    </Text>
+                    {address.Type !== 'Address' && (
+                      <>
+                        <ScreenReaderOnly>{`press enter for these addresses`}</ScreenReaderOnly>
+                        <Icon name="chevronRight" />
+                      </>
+                    )}
+                  </ListItem>
+                ))}
+              </List>
+            </ListWrapper>
+          </>
         )}
         {error && <ErrorText>{error}</ErrorText>}
       </>
