380807891b7aa63941fe89e214bfa6fac1d8c31e
John Uff <John.Uff@cancer.org.uk>
2021-07-01 14:14:13 +0100

    PLB-97 added documentation about component

--- src/components/LegendWrapper/index.tsx
+++ src/components/LegendWrapper/index.tsx
@@ -15,12 +15,17 @@ export type LegendWrapperProps = {
   errorMessage?: string;
   /** flag for error styling */
   hasError?: boolean;
-  /** when required (required) appears in label */
+  /** when required (required) appears in legend */
   required?: boolean;
   /** hint text */
   hintText?: ReactNode | string;
 };
-
+/**
+ * LegendWrapper allows the user to add an outer label for a checkbox / group of checkboxes / radio buttons or a group of radio buttons.
+ *
+ * hasError and errorMessage props are just props that can be passed into the component.
+ * There is no functionality checking if the children are valid/required etc this functionality will be needed to ne added outside of this component.
+ */
 export const LegendWrapper: FC<LegendWrapperProps> = ({
   children,
   legendText,
