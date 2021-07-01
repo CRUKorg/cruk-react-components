dad3261295d8ad88715359ef01e1281bb09da0ef
John Uff <John.Uff@cancer.org.uk>
2021-07-01 14:33:59 +0100

    PLB-97 updating stories with multiple radios/checboxes, hintText and missed legendText

--- src/components/LegendWrapper/LegendWrapper.stories.tsx
+++ src/components/LegendWrapper/LegendWrapper.stories.tsx
@@ -19,13 +19,19 @@ export const LegendWrapperDefault: Story<LegendWrapperProps> = Template.bind({})
 LegendWrapperDefault.storyName = 'LegendWrapper';
 LegendWrapperDefault.args = {
   legendText: 'Legend Example',
+  hintText: 'This is hint text'
 };
 
 const TemplateWithRadio: Story<LegendWrapperProps> = args => (
   <LegendWrapper {...args}>
-    <Box marginBottom="none">
+    <Box>
       <Radio name="example1" value="one">
         Option one
+      </Radio>
+    </Box>
+    <Box marginBottom="none">
+      <Radio name="example1" value="two">
+        Option two
       </Radio>
     </Box>
   </LegendWrapper>
@@ -35,6 +41,7 @@ export const LegendWrapperRadio: Story<LegendWrapperProps> = TemplateWithRadio.b
 LegendWrapperRadio.storyName = 'LegendWrapper with Radio Button';
 LegendWrapperRadio.args = {
   legendText: 'Legend Example',
+  hintText: 'This is hint text'
 };
 
 export const LegendWrapperRadioError: Story<LegendWrapperProps> = TemplateWithRadio.bind({});
@@ -43,10 +50,14 @@ LegendWrapperRadioError.args = {
   legendText: 'Legend Example',
   hasError: true,
   errorMessage: 'Error message',
+  hintText: 'This is hint text'
 };
 
 const TemplateWithCheckbox: Story<LegendWrapperProps> = args => (
   <LegendWrapper {...args}>
+    <Box>
+      <CheckBox disabled={false} value="value" />
+    </Box>
     <Box marginBottom="none">
       <CheckBox disabled={false} value="value" />
     </Box>
@@ -56,23 +67,30 @@ const TemplateWithCheckbox: Story<LegendWrapperProps> = args => (
 export const LegendWrapperCheckbox: Story<LegendWrapperProps> = TemplateWithCheckbox.bind({});
 LegendWrapperCheckbox.storyName = 'LegendWrapper with Checkbox';
 LegendWrapperCheckbox.args = {
-  legend: 'Legend Example',
+  legendText: 'Legend Example',
+  hintText: 'This is hint text'
 };
 
 export const LegendWrapperCheckboxError: Story<LegendWrapperProps> = TemplateWithCheckbox.bind({});
 LegendWrapperCheckboxError.storyName = 'LegendWrapper with Error and Checkbox';
 LegendWrapperCheckboxError.args = {
-  legend: 'Legend Example',
+  legendText: 'Legend Example',
   hasError: true,
   errorMessage: 'Error message',
+  hintText: 'This is hint text'
 };
 
 const TemplateWithSU2C: Story<LegendWrapperProps> = args => (
   <ThemeProvider theme={su2cTheme}>
     <LegendWrapper {...args}>
-      <Box marginBottom="none">
-        <Radio name="example1" value="one">
+      <Box>
+        <Radio name="example2" value="one">
           Option one
+        </Radio>
+      </Box>
+      <Box marginBottom="none">
+        <Radio name="example2" value="two">
+          Option two
         </Radio>
       </Box>
     </LegendWrapper>
@@ -82,7 +100,8 @@ const TemplateWithSU2C: Story<LegendWrapperProps> = args => (
 export const SU2CLegendWrapperRadioError: Story<LegendWrapperProps> = TemplateWithSU2C.bind({});
 SU2CLegendWrapperRadioError.storyName = 'SU2C LegendWrapper with Error and Radio Button';
 SU2CLegendWrapperRadioError.args = {
-  legend: 'Legend Example',
+  legendText: 'Legend Example',
   hasError: true,
   errorMessage: 'Error message',
+  hintText: 'This is hint text'
 };
