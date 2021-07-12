1d930f204e4b008a2b7bf284d30f2a69b3395132
John Uff <John.Uff@cancer.org.uk>
2021-07-12 09:31:36 +0100

    PLB-40 removal of overwriting font weight and fixing spelling mistake in ternary

--- src/components/Checkbox/styles.ts
+++ src/components/Checkbox/styles.ts
@@ -66,7 +66,7 @@ export const StyledLabel = styled.label<StyledLabelProps>`
       : theme.colors.inputBorder};
   cursor: pointer;
   display: block;
-  font-weight: ${({ theme, checked }) => (checked || !theme.utilities.useDefaultFocusRec ? 'bold' : 'normal')};
+  font-weight: ${({ theme, checked }) => (checked || !theme.utilities.useDefaultFocusRect ? theme.typography.fontWeightHeavy : 'normal')};
   color: ${({ theme, disabled }) => (disabled ? theme.colors.disabled : theme.colors.textDark)};
   padding: ${({ theme }) =>
     `calc( (${BUTTON_HEIGHT} - ( ${theme.utilities.inputBorderWidth} * 2) - ${theme.typography.lineHeight} ) / 2) ${theme.spacing.m} calc( (${BUTTON_HEIGHT} - ( ${theme.utilities.inputBorderWidth} * 2) - ${theme.typography.lineHeight} ) / 2) ${theme.spacing.xl}`};
@@ -81,8 +81,6 @@ export const StyledLabel = styled.label<StyledLabelProps>`
       ? null
       : css`
           min-height: 2rem;
-          font-weight: ${({ theme }) => theme.typography.fontWeightHeavy};
-
           &:hover ${CheckWrapper} ${Check} {
             border: solid 1px
               ${({
