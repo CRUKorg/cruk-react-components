5fe30a774565c9f853933ffa19c2c298138cd05a
John Uff <John.Uff@cancer.org.uk>
2021-07-12 11:29:52 +0100

    Revert "PLB-40 adding internal state to control whether input has been checked"
    This reverts commit 57f78b64c82fd3d1dae98d5dfe10c4f3f1183e28.

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, InputHTMLAttributes, Ref, forwardRef, useState } from 'react';
+import React, { FC, InputHTMLAttributes, Ref, forwardRef } from 'react';
 import { useTheme, ThemeProvider } from 'styled-components';
 
 import defaultTheme from 'src/themes/cruk';
@@ -26,20 +26,15 @@ const Checkbox: FC<CheckBoxProps> = forwardRef((props: CheckBoxProps, ref?: Ref<
     ...foundTheme,
   };
   const { children, ...propsWithoutChildren } = props;
-  const [isChecked, setIsChecked] = useState(false);
+
   return (
     <ThemeProvider theme={theme}>
       <StyledLabel
         hasError={props.hasError || !!props.errorMessage || false}
-        checked={props.checked || props.defaultChecked || isChecked || false}
+        checked={props.checked || props.defaultChecked || false}
         disabled={props.disabled || false}
       >
-        <StyledInput
-          onChange={e => setIsChecked(e.target.checked)}
-          {...propsWithoutChildren}
-          type="checkbox"
-          ref={ref}
-        />
+        <StyledInput {...propsWithoutChildren} type="checkbox" ref={ref} />
         <SelectedBorder></SelectedBorder>
         {children || props.value}
         {theme.utilities.useDefaultFromControls ? null : (
