6b7bbcef56096248caa8a7cc91da31decbd0f97e
dhwanijoshi <dhwani.joshi@astrazeneca.com>
2021-08-06 12:23:55 +0100

    adding config file for eyes added run script to run visual tests

---
+++ applitools.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    appName: 'Design System Test',
+    showLogs: false,
+    batchName: 'test-Radio',
+    browser: [
+        {width: 800, height: 600, name: 'firefox'},
+        {width: 1024, height: 768, name: 'chrome'},
+        {width: 1024, height: 768, name: 'safari'},
+        {width: 800, height: 600, name: 'edgechromium'},
+        {
+            deviceName: 'iPhone 11 Pro',
+            screenOrientation: 'landscape' 
+        },
+        {
+            deviceName: 'Galaxy S20',
+            screenOrientation: 'landscape',
+            name: 'chrome' // optional, just to make it explicit this is browser emulation and not a real device. Only chrome is supported for device emulation.
+        }
+    ],  
+    include: /Radio: */ // only to test stories titled Radio
+   // all other configuration variables apply
+}
