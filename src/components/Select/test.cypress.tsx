0447f14d75c87c4de996df342d4377232f492974
dhwanijoshi <dhwani.joshi@astrazeneca.com>
2021-08-18 10:59:33 +0100

    remove unused imports and ids

--- src/components/Select/test.cypress.tsx
+++ src/components/Select/test.cypress.tsx
@@ -5,13 +5,12 @@ import { mount } from '@cypress/react';
 
 import TestWrapper, { TestThemeWrapper } from '../TestWrapper';
 import { Select, Box, Button, su2cTheme, crukTheme } from '../';
-import { delay } from 'cypress/types/bluebird';
 
 const content = () => {
   return (
     <>
       <Box>
-        <Select id="select1" value="" label="Disabled option" onChange={event => {}}>
+        <Select value="" label="Disabled option" onChange={event => {}}>
           <option disabled value="">
             --Please choose an option--
           </option>
@@ -20,7 +19,7 @@ const content = () => {
         </Select>
       </Box>
       <Box>
-        <Select id="select2" value="" label="Disabled control" onChange={event => {}} disabled>
+        <Select value="" label="Disabled control" onChange={event => {}} disabled>
           <option disabled value="">
             --Please choose an option--
           </option>
@@ -29,14 +28,14 @@ const content = () => {
         </Select>
       </Box>
       <Box>
-        <Select id="select3" required hasError={true} label="Has error" onChange={event => {}}>
+        <Select required hasError={true} label="Has error" onChange={event => {}}>
           <option value="dog">Dog</option>
           <option value="red_panda">Red panda</option>
           <option value="axolotl">Axolotl</option>
         </Select>
       </Box>
       <Box>
-        <Select id="select4" required errorMessage="This felid is required ☹️" label="Error message" onChange={event => {}}>
+        <Select required errorMessage="This felid is required ☹️" label="Error message" onChange={event => {}}>
           <option value="cat">Cat</option>
         </Select>
       </Box>
@@ -47,7 +46,7 @@ const content = () => {
 const selectSection = () =>{
   return (
     <Select label="Test Select Option">
-      <option disabled value>Please select one of the below</option>
+      <option disabled value="">Please select one of the below</option>
       <option value="dog">Dog</option>
       <option value="cat">Cat</option>
     </Select>
@@ -94,9 +93,9 @@ describe('Arrow Keys', ()=>{
   it('expands the listbox and selects the next option.', () => {
     mount(<TestThemeWrapper theme={crukTheme}>{selectSection()}</TestThemeWrapper>)
     cy.get('.sc-dkQUaI').type("{enter}").type("{downarrow}").select("Cat").should('have.value','cat');
- });
+});
 
- it('expands the listbox and selects the previous option.', () => {
+it('expands the listbox and selects the previous option.', () => {
   mount(<TestThemeWrapper theme={crukTheme}>{selectSection()}</TestThemeWrapper>)
   cy.get('.sc-dkQUaI').find('option').then($elm => $elm.get(2).setAttribute('selected',"selected"))
   cy.get('.sc-dkQUaI').type("{enter}").type("{uparrow}").select("Dog").should('have.value','dog');
@@ -108,7 +107,7 @@ describe('Tab', ()=>{
     mount(<TestThemeWrapper theme={crukTheme}>{
       <><Box>
         <Select label="Test Select Option">
-          <option disabled value>Please select one of the below</option>
+          <option disabled value="">Please select one of the below</option>
           <option value="dog">Dog</option>
           <option value="cat">Cat</option>
         </Select>
@@ -122,6 +121,6 @@ describe('Tab', ()=>{
     cy.get('.sc-dkQUaI').tab();
     cy.focused().should('have.text','Click me').tab({shift:true});
     cy.focused().should('have.descendants','option').and('include.text','Please select one of the below')
-   })
- });
+  })
+});
 
