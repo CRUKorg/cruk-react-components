f96de403df00ab0884e06c67123cbfc79a888e12
Elliot <elliotrevan@gmail.com>
2021-09-24 21:52:47 +0100

    get webpack dev server, cypress unit tests and storybook working with webpack 5

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,26 +1,22 @@
 const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
-const preprocessor = require('@cypress/webpack-preprocessor');
-const options = { webpackOptions: require('../../webpack.config.test.js') };
+// const preprocessor = require('@cypress/webpack-preprocessor');
+// const options = { webpackOptions: require('../../webpack.config.test.js') };
 
 module.exports = (on, config) => {
   addMatchImageSnapshotPlugin(on, config);
-  
+
   if (config.testingType === 'component') {
-    const { startDevServer } = require('@cypress/webpack-dev-server')
-    
+    const { startDevServer } = require('@cypress/webpack-dev-server');
+
     // Your project's Webpack configuration
-    const webpackConfig = require('../../webpack.config.test.js')
-    
-    on('file:preprocessor', preprocessor(options));
-    on('dev-server:start', (options) =>
-      startDevServer({ options, webpackConfig })
-    )
+    const webpackConfig = require('../../webpack.config.test.js');
+
+    // on('file:preprocessor', preprocessor(options));
+    on('dev-server:start', options => startDevServer({ options, webpackConfig }));
   }
 
   return config;
 };
-
-
 
 // module.exports = (on, config) => {
 //   addMatchImageSnapshotPlugin(on, config);
