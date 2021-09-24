b2b6cf4163da2a0cf15b938662c4e99e318d842b
Elliot <elliotrevan@gmail.com>
2021-09-24 22:02:39 +0100

    remove all the unused cypress webpack pre processor stuff

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,4 @@
 const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
-// const preprocessor = require('@cypress/webpack-preprocessor');
-// const options = { webpackOptions: require('../../webpack.config.test.js') };
 
 module.exports = (on, config) => {
   addMatchImageSnapshotPlugin(on, config);
@@ -10,27 +8,8 @@ module.exports = (on, config) => {
 
     // Your project's Webpack configuration
     const webpackConfig = require('../../webpack.config.test.js');
-
-    // on('file:preprocessor', preprocessor(options));
     on('dev-server:start', options => startDevServer({ options, webpackConfig }));
   }
 
   return config;
 };
-
-// module.exports = (on, config) => {
-//   addMatchImageSnapshotPlugin(on, config);
-
-//   if (config.testingType === 'component') {
-//     const { startDevServer } = require('@cypress/webpack-dev-server')
-
-//     // Your project's Webpack configuration
-//     const webpackConfig = require('../../webpack.config.test.js')
-
-//     on('dev-server:start', (options) =>
-//       startDevServer({ options, webpackConfig })
-//     )
-//   }
-
-//   return config
-// }
