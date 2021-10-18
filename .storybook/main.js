2e798a438c1e7b29ddaf198e2e05018b06ecc3ae
Elliot <elliotrevan@gmail.com>
2021-10-18 14:09:23 +0100

    fix storybook dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,27 +1,34 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  stories: ['../src/components/*/*.stories.mdx', '../src/components/*/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    "../src/components/*/*.stories.mdx",
+    "../src/components/*/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@react-theming/storybook-addon',
-    '@storybook/addon-a11y',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@react-theming/storybook-addon",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
   ],
   core: {
-    builder: 'webpack5',
+    builder: "webpack5",
   },
-  webpackFinal: async config => {
+  webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       use: [
         {
-          loader: require.resolve('ts-loader'),
+          loader: require.resolve("ts-loader"),
         },
       ],
     });
-    config.resolve.extensions.push('.ts', '.tsx');
-    config.resolve.modules = [...(config.resolve.modules || []), path.resolve('./')];
+    config.resolve.extensions.push(".ts", ".tsx");
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve("./"),
+    ];
     return config;
   },
 };
