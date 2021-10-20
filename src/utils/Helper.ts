625e5fc2a88b67608f6b3f423e8883ba3bc60cad
John Uff <John.Uff@cancer.org.uk>
2021-10-20 12:50:05 +0100

    var' declarations put at the top of the function scope

--- src/utils/Helper.ts
+++ src/utils/Helper.ts
@@ -17,10 +17,11 @@ export const formatMoney = (value: number): string => {
 };
 
 export const numberWithCommas = (n?: number | string): string => {
+  let parts;
   if (!n) {
     return '';
   }
-  var parts = n.toString().split('.');
+  parts = n.toString().split('.');
 
   return `${parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')}${parts[1] ? '.' + parts[1] : ''}`;
 };
