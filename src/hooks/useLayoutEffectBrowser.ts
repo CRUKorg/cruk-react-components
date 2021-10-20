0d88cbf57d0f4b1ce92007dc1d63e6004b42eaf6
John Uff <John.Uff@cancer.org.uk>
2021-10-20 14:35:15 +0100

    removal of export

--- src/hooks/useLayoutEffectBrowser.ts
+++ src/hooks/useLayoutEffectBrowser.ts
@@ -5,7 +5,7 @@ const isBrowser = typeof window !== `undefined`;
 // The sole purpose of this hook is to silence pointless warnings in next.js when using
 // useLayoutEffect about it not being isomorphic which is the whole point of the hook
 // which waits for components to be mounted to the DOM for scroll handlers etc.
-export function useLayoutEffectBrowser(action: EffectCallback, deps: DependencyList) {
+function useLayoutEffectBrowser(action: EffectCallback, deps: DependencyList) {
   if (!isBrowser) return null;
   return useLayoutEffect(action, deps);
 }
