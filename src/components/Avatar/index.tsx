d55632c679db73f86249b200151daeade162469d
John Uff <John.Uff@cancer.org.uk>
2021-10-22 14:25:43 +0100

    fixing defaultProp "size" has no corresponding propTypes declaration

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -18,7 +18,7 @@ export type AvatarProps = ImgHTMLAttributes<HTMLElement> & {
  *
  * You can use an avatar to display ownership of an item of content. If you pass a URL of an image that will be displayed otherwise the first letter of the name will be used to display a branded avatar.
  */
-const Avatar: FC<AvatarProps> = ({ url, name, size, alt = '', ...rest }) => {
+const Avatar: FC<AvatarProps> = ({ url, name, size = 'm', alt = '', ...rest }) => {
   const foundTheme = useTheme();
   const theme = {
     ...defaultTheme,
@@ -37,10 +37,6 @@ const Avatar: FC<AvatarProps> = ({ url, name, size, alt = '', ...rest }) => {
   };
 
   return <StyledAvatar {...rest} size={theme.avatar[size || 'm']} src={avatarUrl()} alt={alt} />;
-};
-
-Avatar.defaultProps = {
-  size: 'm',
 };
 
 export default Avatar;
