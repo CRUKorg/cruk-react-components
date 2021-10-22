19725938e209d44d12e42de5d58fd08a94c5ed4c
John Uff <John.Uff@cancer.org.uk>
2021-10-22 11:56:48 +0100

    fixing  Unsafe member access .breakpoint on an `any` value

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -1,17 +1,24 @@
 import styled, { css } from 'styled-components';
 import spacing, { SpacingProps } from 'src/components/Spacing';
+import { ThemeType } from 'src/types';
 
 type FlexProps = SpacingProps & {
   css: string;
 };
 
+type ThemeProps = {
+  theme: ThemeType
+};
+
 const Flex = styled.div<FlexProps>`
   display: block;
-  @media (min-width: ${({ theme }) => theme.breakpoint.tablet}) {
+  @media (min-width: ${({ theme }: ThemeProps) => theme.breakpoint.tablet}) {
     display: flex;
   }
-  ${props => (css as any)([props.css])}
-  ${props => spacing(props, props.theme)}
+  ${(props) => props.css && css`
+    ${props.css}
+  `}};
+  ${(props) => spacing(props, props.theme)}
 `;
 
 export default Flex;
