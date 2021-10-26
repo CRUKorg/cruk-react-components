3af7f8eff6cfc602230190ebb2f95058ba24ac76
John Uff <John.Uff@cancer.org.uk>
2021-10-26 14:38:53 +0100

    fixing  Don't use `{}` as a type. `{}` actually means "any non-nullish value".

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -9,7 +9,7 @@ import { ScreenReaderOnly, Spinner } from "./styles";
  *
  * Loaders are used after some user interaction that we assume will take some time to complete. They inform the user that their request is beeing processed and that they should wait.
  */
-const Loader: FC<{}> = () => {
+const Loader: FC = () => {
   const foundTheme = useTheme();
   const theme = {
     ...defaultTheme,
