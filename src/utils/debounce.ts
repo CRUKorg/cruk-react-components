4c09253148ae07421d0855d200691b6b45690410
John Uff <John.Uff@cancer.org.uk>
2021-10-26 15:32:41 +0100

    fixing adding eslint disable comments for  Don't use `Function` as a type. The `Function` type accepts any function-like value. and  Do not use "@ts-ignore" because it alters compilation errors

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,9 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/ban-types
 export default (delay: number, callback: Function) => {
   let timerId: number | null;
   return (...args: any[]) => {
     if (timerId) {
       clearTimeout(timerId);
     }
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     timerId = setTimeout(() => {
       callback(...args);
