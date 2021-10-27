946075ea11c6d62f0174af59154a2c734731e2cc
John Uff <John.Uff@cancer.org.uk>
2021-10-27 13:59:04 +0100

    Adding SU2C colour to Avatar styles

--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -1,7 +1,10 @@
 import styled from "styled-components";
+import { ThemeType, ColorKeyType } from "src/types";
 
 type AvatarStyledProps = {
   size?: string;
+  textColor?: string;
+  theme: ThemeType;
 };
 
 export const StyledAvatar = styled.img<AvatarStyledProps>`
@@ -9,6 +12,10 @@ export const StyledAvatar = styled.img<AvatarStyledProps>`
   height: ${({ size }) => size};
   object-fit: cover;
   width: ${({ size }) => size};
+  color: ${({ theme: { colors }, textColor }: AvatarStyledProps) =>
+    textColor && typeof colors[textColor as ColorKeyType] !== "undefined"
+      ? colors[textColor as ColorKeyType]
+      : textColor || colors.textOnPrimary};
 `;
 
 export default StyledAvatar;
