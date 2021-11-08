1effc4154fc0447fcf6c34ef2aa1e663a81a30f9
Elliot <elliotrevan@gmail.com>
2021-11-08 15:25:40 +0000

    linting and getting tests and local server working with the new webpack dev server

--- webpack.config.docs.js
+++ webpack.config.docs.js
@@ -1,27 +1,37 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const createWebpackConfig = require('./webpack.common').createWebpackConfig;
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const createWebpackConfig = require("./webpack.common").createWebpackConfig;
 
 module.exports = createWebpackConfig({
-  mode: 'development',
-  entry: './src/docs',
+  mode: "development",
+  entry: "./src/docs",
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, "public"),
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/docs/index.html',
-      filename: './index.html',
+      template: "./src/docs/index.html",
+      filename: "./index.html",
     }),
   ],
   // webpack-dev-server configuration
   devServer: {
-    host: '0.0.0.0',
-    contentBase: path.resolve(__dirname, './src'),
-    watchContentBase: true,
+    host: "0.0.0.0",
     port: 8080,
-    hot: true,
     historyApiFallback: true,
     open: true,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+    static: {
+      directory: path.resolve(__dirname, "static"),
+      staticOptions: {},
+      publicPath: "/public/",
+      serveIndex: true,
+      watch: true,
+    },
   },
 });
