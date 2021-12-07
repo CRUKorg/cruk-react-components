fb0b9f2bb6a02f9d8ee0bcabda1c420221a03ece
Elliot <elliotrevan@gmail.com>
2021-12-07 11:11:01 +0000

    fix ts bug

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import spacing, { SpacingProps } from "./Spacing";
 import { ThemeType } from "../types";
 
