a9ab956745f07efe544c756cff821d25b24a0045
develliot <elliot.revan@cancer.org.uk>
2022-03-23 10:52:27 +0000

    ignoring antiquated default props eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     "jest/expect-expect": [0],
     "jest/valid-expect": [0],
     "import/no-named-as-default": [0],
+    "react/require-default-props": [0],
   },
   ignorePatterns: ["cypress", "*.config.js", "webpack.*.js", "node_modules"],
 };
