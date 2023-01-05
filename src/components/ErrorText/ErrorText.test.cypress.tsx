333ce8e9f3fa183e33b4ef2f94d59992a54e9d1d
develliot <elliot.revan@cancer.org.uk>
2023-01-05 15:43:18 +0000

    attempt to fix types issue

--- src/components/ErrorText/ErrorText.test.cypress.tsx
+++ src/components/ErrorText/ErrorText.test.cypress.tsx
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 import React from "react";
-import { mount } from "@cypress/react";
+import { mount } from "cypress/react";
 
 import TestWrapper, { TestThemeWrapper } from "../TestWrapper";
 import { ErrorText, crukTheme, su2cTheme } from "..";
