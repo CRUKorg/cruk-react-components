50e62fcd46534ae214408dfd8f37023c75892856
Elliot Revan <elliot.revan@cancer.org.uk>
2025-12-12 21:57:04 +0000

    refactor: standardize text color naming convention in Box component tests

--- src/components/Box/Box.spec.tsx
+++ src/components/Box/Box.spec.tsx
@@ -10,19 +10,19 @@ function component() {
     <>
       <Box>This is box</Box>
       <Box backgroundColor="primary">
-        <Text textColor="textOnPrimary">This is box</Text>
+        <Text textColor="text-on-primary">This is box</Text>
       </Box>
       <Box backgroundColor="secondary">
-        <Text textColor="textOnSecondary">This is box</Text>
+        <Text textColor="text-on-secondary">This is box</Text>
       </Box>
       <Box backgroundColor="secondary">
-        <Text textColor="textOnSecondary">default spacing</Text>
+        <Text textColor="text-on-secondary">default spacing</Text>
       </Box>
       <Box backgroundColor="secondary" paddingVertical="xl" paddingBottom="xs">
-        <Text textColor="textOnSecondary">{`paddingVertical="xl" paddingBottom="xs"`}</Text>
+        <Text textColor="text-on-secondary">{`paddingVertical="xl" paddingBottom="xs"`}</Text>
       </Box>
       <Box backgroundColor="primary" marginVertical="l" marginLeft="s">
-        <Text textColor="textOnPrimary">{`marginVertical="l" marginLeft="s"`}</Text>
+        <Text textColor="text-on-primary">{`marginVertical="l" marginLeft="s"`}</Text>
       </Box>
     </>
   );
