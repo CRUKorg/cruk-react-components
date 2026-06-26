2934908a967aec0bb444253160797f2685851a39
Elliot Revan <elliot.revan@cancer.org.uk>
2026-06-26 17:37:30 +0100

    refactor: optimize address search with debouncing and restructure search function

--- src/components/AddressLookup/index.tsx
+++ src/components/AddressLookup/index.tsx
@@ -90,40 +90,37 @@ export const AddressLookup = ({
     setAddressOptions([]);
   };
 
-  const search = useCallback(
-    (query: string, id = "") => {
-      if (query.length === 0) return setAddressOptions([]);
-      let searchUrl = `${FIND_URL}?Key=${apiKey}&Text=${query}&Container=${id}`;
-      if (countries !== undefined) {
-        searchUrl = `${searchUrl}&Countries=${countries.join(",")}`;
-      }
-      fetch(searchUrl)
-        .then((res: Response) => {
-          if (!res.ok) throw new Error("Something went wrong please try again");
-          return res.json();
-        })
-        .then((data: { Items: AddressOptionsType[] }) => {
-          // Occasionally get the error "The query didn't respond fast enough, it may be too complex."
-          // returned with a 200 response. Example query "n17 6t"
-          if (data?.Items?.length > 0 && data.Items[0].Error)
-            throw new Error("Something went wrong please try again");
-          setAddressOptions(data.Items || []);
-          return null;
-        })
-        .catch((err) => onAddressError(err as Error));
-      return null;
-    },
-    [apiKey, countries, onAddressError],
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const searchDebounced = useCallback(
+    // eslint-disable-next-line react-hooks/use-memo
+    debounce(500, (query: string) => {
+      search(query);
+    }),
+    [],
   );
 
-  const searchDebounced = useCallback(
-    (query: string) => {
-      debounce(500, () => {
-        search(query);
-      });
-    },
-    [search],
-  );
+  const search = (query: string, id = "") => {
+    if (query.length === 0) return setAddressOptions([]);
+    let searchUrl = `${FIND_URL}?Key=${apiKey}&Text=${query}&Container=${id}`;
+    if (countries !== undefined) {
+      searchUrl = `${searchUrl}&Countries=${countries.join(",")}`;
+    }
+    fetch(searchUrl)
+      .then((res: Response) => {
+        if (!res.ok) throw new Error("Something went wrong please try again");
+        return res.json();
+      })
+      .then((data: { Items: AddressOptionsType[] }) => {
+        // Occasionally get the error "The query didn't respond fast enough, it may be too complex."
+        // returned with a 200 response. Example query "n17 6t"
+        if (data?.Items?.length > 0 && data.Items[0].Error)
+          throw new Error("Something went wrong please try again");
+        setAddressOptions(data.Items || []);
+        return null;
+      })
+      .catch((err) => onAddressError(err as Error));
+    return null;
+  };
 
   const getAddress = (id: string) => {
     fetch(`${RETRIEVE_URL}?Key=${apiKey}&Id=${id}`)
