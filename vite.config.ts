2c59c832c53c8d83414861761b18fdc5d470be13
Elliot Revan <elliot.revan@cancer.org.uk>
2026-08-21 12:52:28 +0100

    Remove legacy plugin from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
-import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig(({ mode }) => ({
-  plugins: [legacy({ targets: ["defaults", "not IE 11"] })],
   define: {
     "process.env.NODE_ENV": `'${mode || "production"}'`,
   },
